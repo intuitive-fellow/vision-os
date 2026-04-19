a5e776606cd965da5258b8d071ce641f2e4267f4
Musashi <129923924+intuitive-fellow@users.noreply.github.com>
2026-04-19 13:57:48 +0530

    Updated JWT configs

--- config/settings/jwt.py
+++ config/settings/jwt.py
@@ -1,10 +1,11 @@
 from datetime import timedelta
+from config.env import env
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=env.int("JWT_ACCESS_TOKEN_LIFETIME")),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=env.int("JWT_REFRESH_TOKEN_LIFETIME")),
+    "ROTATE_REFRESH_TOKENS": env.bool("JWT_ROTATE_REFRESH_TOKENS"),
+    "BLACKLIST_AFTER_ROTATION": env.bool("JWT_BLACKLIST_AFTER_ROTATION"),
     "AUTH_HEADER_TYPES": ("Bearer",),
-    "UPDATE_LAST_LOGIN": True,
+    "UPDATE_LAST_LOGIN": env.bool("JWT_UPDATE_LAST_LOGIN"),
 }
